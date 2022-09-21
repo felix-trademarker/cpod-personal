2238365c87426219a57bd2df6ac9c8885c1e1302
Felix <felix.bongo@tweeklabs.com>
2022-09-21 16:15:28 +0800

    update

--- config/variables.js
+++ config/variables.js
@@ -9,6 +9,6 @@ module.exports = {
                       },
     mongoDB         : process.env.mongoDB ? process.env.mongoDB : 'chinesepod',
     filePathUpload  : (ENV === 'prod' ? process.env.uploadFilePath : process.env.uploadFilePathDev),
-    ipAddresses     : ['103.104.17','211.20.18','211.72.53','122.116.227','122.52.119','61.244.218','50.74.20','127.0.0','::1'],
+    ipAddresses     : ['103.104.17','211.20.18','211.72.53','122.116.227','122.52.119','61.244.218','50.74.20','143.44.162','127.0.0','::1'],
     
 };
